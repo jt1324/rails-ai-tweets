d31f62939bcca36b3001f75fee511017797f9bd4
jean toneli <jeantoneli@gmail.com>
2026-01-15 21:13:50 +0000

    set up done before authentication

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  resources :tweets, only: [:show, :new, :create]
+  def new
+    @tweet = Tweet.new
+  end
+
 end
